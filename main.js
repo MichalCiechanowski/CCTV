4e2acaf612362ce43f4a629e4eb5de421f9c221f
ciechanowski62@gmail.com <ciechanowski62@gmail.com>
2021-04-08 15:32:55 +0200

    commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function themchange_to_dark() {
+    document.getElementById("back1").style.background = "black"
+    document.getElementById("back2").style.background = "black"
+    document.getElementById("back3").style.background = "black"
+    document.getElementById("back4").style.background = "black"
+    document.getElementById("back5").style.background = "black"
+    document.getElementById("back6").style.background = "black"
+}
+
+function themchange_to_light() {
+    document.getElementById("back1").style.background = "white"
+    document.getElementById("back2").style.background = "white"
+    document.getElementById("back3").style.background = "white"
+    document.getElementById("back4").style.background = "white"
+    document.getElementById("back5").style.background = "white"
+    document.getElementById("back6").style.background = "white"
+}
